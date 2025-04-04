e86c67571ac8920ba584a7cb76440bfcdfad79bb
nothing-c <halskdjf@gmail.com>
2025-04-04 18:52:48 -0500

    gitignore + f# boiler

---
+++ Program.fs
@@ -0,0 +1,10 @@
+﻿module Pomotok =
+    // One call to start a timer (returns a piece of JS that sleeps for N seconds, then makes a request to pull the TikTok)
+    // One call to return a div that covers the whole screen, plays embedded TikTok (or Nyancat) for N seconds
+
+
+
+module Passgen = 
+
+// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"
