c3274d17b58089f3f48e2926a8c024877b95893c
nothing-c <halskdjf@gmail.com>
2025-04-04 20:31:36 -0500

    Add brainrot & worktime endpoints

--- Program.fs
+++ Program.fs
@@ -2,11 +2,21 @@
 open System.Security.Cryptography
 open System.Net.Http
 
-module Pomotok =
-    let endpoint = "/pomotok"
-    let brainrot = "<iframe id=\"brainrot\" src=\"https://www.nyan.cat/\" title=\"Brainrot!\" style=\"position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;\">Your browser doesn't support iframes, back to work!</iframe>"
+module Pomorot =
+    let pomoendpoint = "/pomo"
+    let rotendpoint = "/rot"
+    let breaktime = string (5*60)                //https://en.wikipedia.org/wiki/Pomodoro_Technique; 5m break
+    let worktime = 25*60                //Have to be strings because we're interpolating
+    let brainrot =
+        $"<div align=\"center\" hx-get=\"{pomoendpoint} hx-trigger=\"load delay:{breaktime}\" hx-swap=\"outerHTML\"><iframe id=\"brainrot\" src=\"https://www.nyan.cat/\" title=\"Brainrot!\" style=\"position:fixed ; top:0 ; left:0 ; bottom:0 ; right:0 ; width:100%% ; height:100%% ; border:none ; margin:0 ; padding:0 ; overflow:hidden ; z-index:999999 ;\">Your browser doesn't support iframes, back to work!</iframe></div>"
+    let work =
+        let clocktime = DateTime.UtcNow.AddMinutes(worktime).ToString("HH:mm")
+        let strworktime = string worktime
+        $"<div align=\"center\" hx-get=\"{rotendpoint}\" hx-trigger=\"load delay:{strworktime}\" hx-swap=\"outerHTML\"><h1>Working until {clocktime}...</h1></div>"
     let htmxsrc = "<script src=\"https://unpkg.com/htmx.org@2.0.4\" integrity=\"sha384-HGfztofotfshcF7+8n44JQL2oJmowVChPTg48S+jvZoztPfvwD79OC/LTtG6dMp+\" crossorigin=\"anonymous\"></script>"
     // let htmx endpoint = $"hx-get=\"{endpoint}\" hx-trigger=\"[???]\" hx-swap=\"outerHTML\""
+    // regular page: big thing that says: work til {time}
+    // brainrot page: nyancat
     // One call to start a timer (returns a piece of JS that sleeps for N seconds, then makes a request to pull the TikTok)
     // setTimeout(lambda goes here, t) where t is in mills
     // One call to return a div that covers the whole screen, plays embedded TikTok (or Nyancat) for N seconds
