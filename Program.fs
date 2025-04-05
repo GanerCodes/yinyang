1e7311fea9c461db1992ea389068435b9eb16f07
nothing-c <halskdjf@gmail.com>
2025-04-04 20:14:41 -0500

    Add brainrot iframe & htmx source pull

--- Program.fs
+++ Program.fs
@@ -4,10 +4,14 @@ open System.Net.Http
 
 module Pomotok =
     let endpoint = "/pomotok"
+    let brainrot = "<iframe id=\"brainrot\" src=\"https://www.nyan.cat/\" title=\"Brainrot!\" style=\"position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;\">Your browser doesn't support iframes, back to work!</iframe>"
+    let htmxsrc = "<script src=\"https://unpkg.com/htmx.org@2.0.4\" integrity=\"sha384-HGfztofotfshcF7+8n44JQL2oJmowVChPTg48S+jvZoztPfvwD79OC/LTtG6dMp+\" crossorigin=\"anonymous\"></script>"
+    // let htmx endpoint = $"hx-get=\"{endpoint}\" hx-trigger=\"[???]\" hx-swap=\"outerHTML\""
     // One call to start a timer (returns a piece of JS that sleeps for N seconds, then makes a request to pull the TikTok)
     // setTimeout(lambda goes here, t) where t is in mills
     // One call to return a div that covers the whole screen, plays embedded TikTok (or Nyancat) for N seconds
-    // maybe a usually-invisble div whose properties are modifiable
+    // maybe a usually-invisible div whose properties are modifiable
+    
 
 module Passgen =
     // One call to generate a password
